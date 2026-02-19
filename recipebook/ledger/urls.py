052381dc2de3d75215fbd0bdb4c7ec904bba2bbc
Kenaz <kenaz.celestino@gmail.com>
2026-02-19 20:21:55 +0800

    added Ingredient, Recipe, and RecipeIngredient models

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('recipes/list', list, name='recipe_list'),
-    path('recipe/<int:index>', recipe, name='temporary_url')
+    path('recipe/<int:index>', recipe, name='recipe')
 ]
 
 app_name = "ledger"
