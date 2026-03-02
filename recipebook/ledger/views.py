d48188f6456f152d2414dd7ccc1ba9c24aaafb2d
Kenaz <kenaz.celestino@gmail.com>
2026-03-02 13:03:47 +0800

    Required login for recipe detail view

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import *
 
@@ -11,6 +12,6 @@ class RecipeListView(ListView):
     template_name = 'ledger/list.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'ledger/recipe.html'
