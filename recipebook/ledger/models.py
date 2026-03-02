5186bcb63c80012abb1657a4dd6b072d6e089c32
Kenaz <kenaz.celestino@gmail.com>
2026-03-02 10:00:05 +0800

    created profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -11,6 +21,14 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='recipe'
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
